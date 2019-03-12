5a00f0ac845baaf5919c76c9d50faf81f7d4f98b
jamiejann <jamie.jann@gmail.com>
2019-03-11 18:10:51 -0700

    Refactor + Product ID extraction

--- scraper/import.py
+++ scraper/import.py
@@ -2,6 +2,7 @@
 import urllib2
 import time
 import re
+import notification
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -37,37 +38,50 @@ containers = bs.find_all("div", class_="feed-item")
 #initialize csv file
 filename = "file.csv"
 f = open(filename, "w")
-headers = "brand, original_price, new_price\n"
+headers = "product_id, brand, original_price, new_price\n"
 f.write(headers)
 
 #extracting
 item_number=1
 for container in containers:
+
+    brand_name = extract_brand_name(container)
+    product_id = extract_product_id(container)
+    original_price, new_price = extract_price(container)
+    
+    if new_price == 0:
+        #print("product ID: " + product_id + " item #: " + str(item_number) + "brand: " + brand + "  original price: " + str(original_price))
+        f.write(product_id + "," + brand + "," + original_price.replace(",", "") + "," + str("-") + "\n")
+    else:
+        #print("product ID: " + product_id + " item #: " + str(item_number) + "brand: " + brand + "  original price: " + str(original_price) +"  new price: " + str(new_price))
+        f.write(product_id + "," + brand + "," + original_price.replace(",", "") + "," + new_price.replace(",", "") + "\n")
+
+    item_number = item_number + 1
+    if item_number == 51: break
+
+f.close()
+        
+    
+def extract_brand_name(container):
     brand_container = container.find_all("h3", class_="listing-designer")
     brand = brand_container[0].text.strip()
     brand = re.sub(r'[^a-zA-Z ]+', '',brand)
+    return brand
 
+def extract_product_id(container):
+    product_id_container = container.find_all(class_="heart-follow")
+    product_id = product_id_container[0].get('id')
+    return product_id
+    
+def extract_price(container):
     original_price_container = container.find_all("h3", class_="original-price")
     original_price = original_price_container[0].text.strip()
 
     if(container.find_all("h3", class_="new-price")):
         new_price_container = container.find_all("h3", class_="new-price")
         new_price = new_price_container[0].text.strip()
-
-        print("item #: " + str(item_number) + "brand: " + brand + "  original price: " + str(original_price) +"  new price: " + str(new_price))
-
-        f.write(brand + "," + original_price.replace(",", "") + "," + new_price.replace(",", "") + "\n")
-
-        del new_price
-
+        return original_price, new_price
     else:
-        print("item #: " + str(item_number) + "brand: " + brand + "  original price: " + str(original_price))
-        f.write(brand + "," + original_price.replace(",", "") + "," + str("-") + "\n")
+        new_price = 0
+        return original_price, new_price
 
-    item_number = item_number + 1
-
-    if item_number == 51: break
-
-f.close()
-        
-    
